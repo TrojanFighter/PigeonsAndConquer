dc6f073b577695d807be2808ae2ecc84874116fe
MOVEINMOTION\MusketeerOrder <walkerinchaos@gmail.com>
2018-01-27 11:54:06 -0500

    Local basic defines

---
+++ Assets/Scripts/Define/Fraction.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fraction {
+	public int FractionID;//阵营ID
+	public int FractionsMaxHP;//阵营HP上限
+	public int FractionsInitHP;//阵营初始HP
+	public bool WinConditionHP;//胜利条件：对手HP到0直接胜利？0为不能，1为能
+}
---
+++ Assets/Scripts/Scene/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager Instance;
+	public static Dictionary<int,SoldierType> SoldierTypes;
+	public static Dictionary<int,Fraction> Fractions;
+
+	public Unit Messenger,Knight,Archer,Pikeman;
+	//public GameObject[,] SpawnPointArray;
+	public BasePoint[] BasePointArray;
+	public GameObject GodRayPurple,GodRayGreen,WallOfFire;
+	public static Dictionary<GlobalDefine.Fraction,BasePoint> BasePoints;
+	public Material[] materials;
+
+	public InAudioEvent MessengerDeliverEvent,KnightAttackEvent,ArcherAttackEvent,PickmanAttackEvent,VictoryBlast; 
+	public Animator LeftPanel,RightPanel;
+
+	void Awake(){
+		Instance = this;
+
+		/*if (SpawnPointArray==null) {
+			SpawnPointArray = new GameObject[RoadArray.Length, 2];
+			for (int i = 0; i < RoadArray.Length; i++) {
+				SpawnPointArray [i, 0] = RoadArray [i].transform.GetChild (0).gameObject;
+				SpawnPointArray [i, 1] = RoadArray [i].transform.GetChild (1).gameObject;
+			}
+		}*/
+		SoldierTypes = XMLReader.ReadSoldierTypeFile (Application.dataPath+GlobalDefine.PathDefines.XML_Path+GlobalDefine.FileName.SoldierType);
+		Fractions = XMLReader.ReadFractionsFile (Application.dataPath+GlobalDefine.PathDefines.XML_Path + GlobalDefine.FileName.Fraction);
+		BasePoints = new Dictionary<GlobalDefine.Fraction, BasePoint> ();
+		BasePoints.Add(GlobalDefine.Fraction.One,BasePointArray[0]);
+		BasePoints.Add(GlobalDefine.Fraction.Two,BasePointArray[1]);
+
+
+	}
+
+
+}
