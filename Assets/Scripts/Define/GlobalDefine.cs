e1531ac40f536e02630a89106e200bea69700ae3
Kelly Maddox <kellymaddox08@gmail.com>
2018-01-27 15:22:11 -0500

    Fixing merge

---
+++ Assets/Scripts/Define/GlobalDefine.cs
@@ -0,0 +1,39 @@
+﻿
+public static class GlobalDefine  {
+	public enum UnitClass{
+		Messenger=0,
+		Knight=1,
+		Archer=2,
+		Pikeman=3,
+	}
+	public enum Fraction{
+		Netrual=0,
+		One=1,//默认左边
+		Two=2//默认右边
+	}
+	public enum UnitState
+	{
+		Born=0,
+		Advance,
+		Attack,
+	}
+
+
+	public static class PathDefines{
+		public const string XML_Path="/StreamingAssets/XML/";
+		public const string TROOP_PREFAB="Prefab/Units/";
+		public const string UI_PREFAB="Prefab/UI/";
+	}
+	public static class FileName{
+		public const string Fraction="Fractions.XML";
+		public const string SoldierType="Soldiers.XML";
+
+	}
+	public static class ObjectTag{
+		public const string SpawnPointTag="SpawnPoint";
+		public const string Fraction0Tag="Fraction0Unit";
+		public const string Fraction1Tag="Fraction1Unit";
+		public const string Fraction2Tag="Fraction2Unit";
+
+	}
+}
---
+++ Assets/Scripts/XMLConfig/XMLReader.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Xml;
+
+public static class XMLReader{
+	public static Dictionary<int,SoldierType> ReadSoldierTypeFile(string path)
+	{
+		Dictionary<int,SoldierType> soldierTypeList = new Dictionary<int, SoldierType>();
+		XmlDocument xDoc = new XmlDocument();
+		xDoc.Load(path);
+
+		XmlNamespaceManager xnm = new XmlNamespaceManager(xDoc.NameTable);
+		xnm.AddNamespace("WB", "urn:schemas-microsoft-com:office:spreadsheet");
+		XmlElement root = xDoc.DocumentElement;
+		XmlNodeList rows = root.SelectNodes("/WB:Workbook/WB:Worksheet/WB:Table/WB:Row", xnm);
+
+		for (int i = 3; i < rows.Count; i++)
+		{
+			XmlElement rowNode = rows[i] as XmlElement;
+			if (rowNode != null)
+			{
+				SoldierType newSoldierType = new SoldierType();
+				//评论ID
+				newSoldierType.SoldierTypeID = int.Parse(GetInnerData(rowNode.ChildNodes[0]));
+
+				newSoldierType.SoldierTypeName = GetInnerData(rowNode.ChildNodes[1]);
+
+				newSoldierType.AttackType= int.Parse(GetInnerData(rowNode.ChildNodes[2]));
+
+
+				newSoldierType.NormalMoveSpeed=float.Parse(GetInnerData(rowNode.ChildNodes[3]));
+
+				newSoldierType.NormalHP=int.Parse( GetInnerData(rowNode.ChildNodes[4]));
+
+				newSoldierType.AttackRange =float.Parse( GetInnerData (rowNode.ChildNodes [5]));
+				newSoldierType.AttackTime=float.Parse(GetInnerData(rowNode.ChildNodes[6]));
+
+
+
+				newSoldierType.NormalAttackPower= int.Parse(GetInnerData(rowNode.ChildNodes[7]));
+				newSoldierType.ArmorAgainstNormalAttack =int.Parse( GetInnerData(rowNode.ChildNodes[8]));
+
+				int BeingCapturedOrNotInt=int.Parse(GetInnerData(rowNode.ChildNodes[9]));
+				newSoldierType.StopWhenBeingCaptured = BeingCapturedOrNotInt == 1 ? true : false;
+				int JoinCombatOrNotInt=int.Parse(GetInnerData(rowNode.ChildNodes[10]));
+				newSoldierType.JoinCombat = JoinCombatOrNotInt == 1 ? true : false;
+				newSoldierType.LifeTime=float.Parse(GetInnerData(rowNode.ChildNodes[11]));
+
+				/*string GapTimeString = GetInnerData(rowNode.ChildNodes[10]);
+                string[] GapTimeStringArray = GapTimeString.Split('+');
+
+                for (int f=0;f < GapTimeStringArray.Length;f++) 
+                  {
+                    newComment.CharacterGapTime[f] = float.Parse(GapTimeStringArray[f]);
+                }*/
+
+
+				soldierTypeList.Add(newSoldierType.SoldierTypeID,newSoldierType);
+			}
+		}
+		return soldierTypeList;
+	}
+
+	public static Dictionary<int,Fraction> ReadFractionsFile(string path)
+	{
+		Dictionary<int,Fraction> fractionsList = new Dictionary<int, Fraction>();
+		XmlDocument xDoc = new XmlDocument();
+		xDoc.Load(path);
+
+		XmlNamespaceManager xnm = new XmlNamespaceManager(xDoc.NameTable);
+		xnm.AddNamespace("WB", "urn:schemas-microsoft-com:office:spreadsheet");
+		XmlElement root = xDoc.DocumentElement;
+		XmlNodeList rows = root.SelectNodes("/WB:Workbook/WB:Worksheet/WB:Table/WB:Row", xnm);
+
+		for (int i = 3; i < rows.Count; i++)
+		{
+			XmlElement rowNode = rows[i] as XmlElement;
+			if (rowNode != null)
+			{
+				Fraction newFraction = new Fraction();
+				//评论ID
+				newFraction.FractionID = int.Parse(GetInnerData(rowNode.ChildNodes[0]));
+
+				newFraction.FractionsMaxHP = int.Parse(GetInnerData(rowNode.ChildNodes[1]));
+
+				newFraction.FractionsInitHP= int.Parse(GetInnerData(rowNode.ChildNodes[2]));
+				int WinConditionHPInt=int.Parse( GetInnerData(rowNode.ChildNodes[7]));
+				newFraction.WinConditionHP = WinConditionHPInt == 1 ? true : false;
+				fractionsList.Add(newFraction.FractionID,newFraction);
+			}
+		}
+		return fractionsList;
+	}
+
+
+
+	private static string GetInnerData(XmlNode node) {
+		if (node.ChildNodes[0] != null)
+		{
+			return node.ChildNodes[0].InnerText;
+		}
+		else {
+			return string.Empty;
+		}
+	}
+
+
+}
+
