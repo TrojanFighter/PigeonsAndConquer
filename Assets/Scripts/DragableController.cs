529f08837f8dd56f667d1c8598c0b32bcbc20191
Kelly Maddox <kellymaddox08@gmail.com>
2018-01-27 19:09:52 -0500

    changed to 2d box collider

---
+++ Assets/Scripts/DragableController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragableController : MonoBehaviour {
+	public Transform grabbedUnit;
+
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Vector2 inputPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			RaycastHit2D[] hits = Physics2D.RaycastAll (inputPos, inputPos);
+			if (hits.Length > 0) {
+				grabbedUnit = hits [0].transform;
+				grabbedUnit.GetComponent<DragableObjects> ().Grab();
+			}
+		}
+		if (Input.GetMouseButtonUp (0) && grabbedUnit != null) {
+			grabbedUnit.GetComponent<DragableObjects> ().LetGo();
+			grabbedUnit = null;
+		}
+	}
+}
